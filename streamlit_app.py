4d6972abac2b9450f198f5d4e3ab231ce2ccb1f3
zayouin <20242403@sdhs.gwe.hs.kr>
2024-11-18 15:21:05 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,11 +52,11 @@ if uploaded_file is not None:
     st.markdown("<h3>클래스별 확률:</h3>", unsafe_allow_html=True)
 
     if prediction == labels[0]:
-         st.write("중냉 꿋굿")
+         st.write("중냉임")
     elif prediction == labels[1]:
-         st.write("짜장면은 굿")
+         st.write("짜장임")
     elif prediction == labels[2]:
-         st.write("짬뽕은 맵지만 맛있어!!")
+         st.write("짬뽕임")
 
     for label, prob in zip(labels, probs):
         # HTML 및 CSS로 확률을 시각화
