2238866baccba4d878196be6e323989d23262373
zayouin <20242403@sdhs.gwe.hs.kr>
2024-11-21 09:19:30 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ def display_right_content(labels):
     # 1st Row - Images based on labels
     for i, label in enumerate(labels[:3]):
         with cols[i]:
-            st.image(f"https://i.ibb.co/5Lxb5Hc/image.webp?text={label}", caption=f"이미지: {label}", use_column_width=True)
+            st.image(f"https://i.ibb.co/X49JGLC/image.webp?text={label}", caption=f"이미지: {label}", use_column_width=True)
 
     # 2nd Row - YouTube Videos based on labels
     for i, label in enumerate(labels[:3]):
